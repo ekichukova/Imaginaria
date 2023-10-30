d3718fc9ad8a649f994f2940b45a51d5b58618f6
ekichukova <ekichukova@gmail.com>
2023-10-30 02:35:57 +0200

    Fix request object.

--- TaxesApi/RequestModels/TaxPayerRequest.cs
+++ TaxesApi/RequestModels/TaxPayerRequest.cs
@@ -5,17 +5,17 @@
 
     public class TaxPayerRequest
     {
-      //  [Required]
+        [Required]
         [RegularExpression("^([a-zA-Z]{2,}\\s[a-zA-Z]{1,}'?-?[a-zA-Z]{2,}\\s?([a-zA-Z]{1,})?)", ErrorMessage = "Invalid name.")]
-        public string FullName { get; set; } = "Emiliya Kichukova";
-        public DateTime? BirthDate { get; set; } = DateTime.Now;
+        public string FullName { get; set; }
+        public DateTime? BirthDate { get; set; }
 
-      //  [Required]
+        [Required]
         public decimal? GrossIncome { get; set; }
 
-       // [Required(AllowEmptyStrings = false)]
+        [Required(AllowEmptyStrings = false)]
         [StringLength(10, MinimumLength = 5, ErrorMessage = "The SSN should be between 5 and 10 characters long.")]
-        public string SSN { get; set; } = "123456";
+        public string SSN { get; set; }
         public decimal CharitySpent { get; set; }
 
     }
